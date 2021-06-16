ba280a29e28254ace83d278b42edc40382ef755a
David <dav.llwyr@gmail.com>
2021-06-16 12:08:12 -0600

    add Contador class

---
+++ src/Contador.java
@@ -0,0 +1,19 @@
+public class Contador {
+    private int count;
+    public static void main(String[] args){
+        Contador a = new Contador();
+    }
+    public Contador(){
+
+    }
+
+    public Contador(){
+        count = 0;
+    }
+
+    public Contador(){
+        
+
+    }
+
+}
